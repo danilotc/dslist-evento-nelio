210852f41b9edc6fb684665e4cbb5949d74d6725
Danilo Trindade <danilotrindade87@gmail.com>
2023-05-09 00:52:05 -0300

    Projeto criado
    Esta eh a estrutura inicial do projeto criado na primeira aula do evento promovido pelo Nelio da Dev Superior

---
+++ src/test/java/edu/danilotc/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.danilotc.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
