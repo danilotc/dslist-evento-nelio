a8c1d80bbf62e5530053d22ca6597b6778ec5a9a
Danilo Trindade <danilotrindade87@gmail.com>
2023-05-12 11:57:29 -0300

    Logica de movimentacao implementada
    Aplicamos a logica de movimentacao no GameListService para alterar a posicao do elemento no banco de dados quando um corpo eh recebido na requisicao, este corpo possui duas informacoes, o index do recurso e o index da posicao
    
    No repositorio GameListRepository adicionamos uma consulta personalizada para atualizar a posicao de um elemento com base no id da lista e no id do game usando uma query no padrao ANSI, lembrando que quando nao eh uma operacao de select precisamos adicionar a anotacao Modifying
    
    Criamos tambem uma classe DTO para que o controller possa receber o objeto do servico, isso porque estamos padronizando as camadas do servico para receber um objeto desse tipo, essa classe eh a ReplacementDTO com os dados que serao passados no corpo da requisicao
    
    Por fim implementamos o controlador para receber esses dados a partir de uma rota especifica chamada 'replacement'

--- src/main/java/edu/danilotc/dslist/services/GameListService.java
+++ src/main/java/edu/danilotc/dslist/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import edu.danilotc.dslist.dto.GameListDTO;
 import edu.danilotc.dslist.entities.GameList;
+import edu.danilotc.dslist.projections.GameMinProjection;
 import edu.danilotc.dslist.repositories.GameListRepository;
+import edu.danilotc.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -16,9 +18,27 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for(int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
 }
